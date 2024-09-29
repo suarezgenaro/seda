a868d31713ad690ed7b2075b176cbaf813f0f43a
Genaro Suárez <91069641+suarezgenaro@users.noreply.github.com>
2024-09-29 01:45:03 -0400

    Add files via upload

---
+++ seda/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='seda',
+      version='1.0',
+      description='Spectral Energy Distribution Analyzer: fit atmospheric models to spectra and/or photometry',
+      keywords = ['ultracool dwarfs', 'low mass stars', 'brown dwarfs', 'gas planets', \
+                  'spectroscopy', 'photometry', 'atmospheres', 'astronomy', 'astrophysics'],
+      author='Genaro Suarez',
+      author_email='gsuarez2405@gmail.com',
+      url='https://github.com/rkiman/wdwarfdate',
+      #license='MIT',
+      packages=['seda'],
+      install_requires=['numpy','astropy','matplotlib','scipy'], # complete the list
+#      zip_safe=False,
+#      include_package_data=True)
