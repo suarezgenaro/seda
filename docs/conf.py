84974f3fd4b74f9ea54f9b76f275d8bc3a188213
Genaro Suárez <91069641+suarezgenaro@users.noreply.github.com>
2024-09-30 23:49:12 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../seda/'))
 
 project = 'SEDA'
 copyright = '2024, Genaro Suárez'
