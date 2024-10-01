1b0a9839f9ffb7b8474fe12f2ee8da54988beb08
Genaro Suárez <91069641+suarezgenaro@users.noreply.github.com>
2024-09-30 23:58:01 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../seda/'))
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'SEDA'
 copyright = '2024, Genaro Suárez'
