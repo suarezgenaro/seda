2aaf77ffb61081ccbaef87ac5d16d144402d95e2
Genaro <gsuarez@astro.unam.mx>
2024-10-19 03:05:08 -0400

    test

--- seda/input_parameters.py
+++ seda/input_parameters.py
@@ -21,9 +21,11 @@ class InputData:
 			* 'no': (default) do not fit photometry
 		wl_spectra : float array, optional (required when fit_spectra=='yes')
 			wavelength in um of the spectrum or set of spectra for the fits
-				CAVEAT. When providing more than one spectrum, verify that there is no overlap between spectra
-					Provide the multiple spectra as a list including the individual spectrum (e.g. wl_spectra = [], wl_spectra.append(spectrum_1), etc.).
+				CAVEAT. When providing more than one spectrum, verify that there is no overlap between the spectra
+					Provide the multiple spectra as a list (e.g. wl_spectra = [], wl_spectra.append(spectrum_1), etc.).
 					The input list must have the spectra from shorter to longer wavelength coverage
+		flux_spectra : float array, optional
+			fluxes in erg/cm^2/s/A of the input spectrum or spectra 
 	'''
 #	fit_spectra : string, optional
 #		'yes': (default) fit spectra
