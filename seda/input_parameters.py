ced3d3679ee1304edebd7b2833ad78e9d4cc70fa
Genaro <gsuarez@astro.unam.mx>
2024-10-21 17:10:25 -0400

    test

--- seda/input_parameters.py
+++ seda/input_parameters.py
@@ -120,7 +120,7 @@ class ModelOptions:
 	-----------
 	- model : {``'Sonora_Diamondback'``, ``'Sonora_Elf_Owl'``, ``'LB23'``, ``'Sonora_Cholla'``, ``'Sonora_Bobcat'``, ``'ATMO2020'``, ``'BT-Settl'``, ``'SM08'``}
 		Atmospheric models used in the comparison. Available models: 
-			- ``'Sonora_Diamondback'`` : cloudy (silicate clouds) atmospheric models assuming chemical equilibrium but considering the effect of both clouds and metallicity by Morley et al. (2024). https://ui.adsabs.harvard.edu/abs/2024arXiv240200758M/abstract
+			- ``'Sonora_Diamondback'`` : cloudy (silicate clouds) atmospheric models assuming chemical equilibrium but considering the effect of both clouds and metallicity by Morley et al. (2024). https://ui.adsabs.harvard.edu/abs/2024arXiv240200758M/abstract. https://zenodo.org/records/12735103
 				Parameter coverage: 
 					- wavelength = [0.3, 250] um
 					- Teff = [900, 2400] K in steps of 100 K
