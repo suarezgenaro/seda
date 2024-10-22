777a0552e25e68e99c071d729f22bc755f3addea
Genaro <gsuarez@astro.unam.mx>
2024-10-21 21:07:31 -0400

    test

--- seda/input_parameters.py
+++ seda/input_parameters.py
@@ -133,7 +133,7 @@ class ModelOptions:
 					- logKzz = 2, 4, 7, 8, and 9 (Kzz in cm2/s)
 					- [M/H] = [-1.0, 1.0] (cgs) with values of -1.0, -0.5, +0.0, +0.5, +0.7, and +1.0
 					- C/O = [0.5, 2.5] with steps of 0.5 (relative to solar C/O, assumed as 0.458) (these are the values in the filenames). It corresponds to C/O=[0.22, 1.12] with values of 0.22, 0.458, 0.687, and 1.12 (e.g. 0.5 in the filename means 0.5*0.458=0.22)
-			- ``'LB23'`` : cloudy (water clouds) atmospheric models with equilibrium and non-equilibrium chemistry for Y-dwarf atmospheres by Lacy & Burrows (2023). https://ui.adsabs.harvard.edu/abs/2023ApJ...950....8L/abstract
+			- ``'LB23'`` : cloudy (water clouds) atmospheric models with equilibrium and non-equilibrium chemistry for Y-dwarf atmospheres by Lacy & Burrows (2023). Paper: https://ui.adsabs.harvard.edu/abs/2023ApJ...950....8L/abstract. Data: https://zenodo.org/records/7779180. Extended models: Private communication with authors.
 				Parameter coverage in common for all grids:
 					- wavelength = [0.5, 300] um with 30,000 frequency points evenly spaced in ln(frequency)
 					- R~4340 (average resolving power)
