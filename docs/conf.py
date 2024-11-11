9d2d22bc8521e980e5dbce46699dcc94a8a8d853
Genaro Suárez <91069641+suarezgenaro@users.noreply.github.com>
2024-11-11 16:05:24 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ html_context = {
   #'github_version': 'master/docs/',  
   'github_user': 'suarezgenaro',
   'github_repo': 'seda',
-  'github_version': 'tree/main',
+  'github_version': 'tree/main/docs/',
 }
 
 # -- Options for HTML output
