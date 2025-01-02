c5602b627b3c7041ccfddd23f3479f9a21400059
Genaro <gsuarez2405@gmail.com>
2025-01-02 17:05:22 -0500

    test

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 #sys.path.insert(0, submodule_dir)
 
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../')) # Source code dir relative to this file
 
 project = 'SEDA'
 copyright = '2024, Genaro Suárez'
@@ -27,21 +27,27 @@ release = '1.0'
 
 # -- General configuration
 extensions = ['sphinx_rtd_theme',
-              'sphinx.ext.autodoc',
+              'sphinx.ext.autodoc', # Core library for html generation from docstrings
+              'sphinx.ext.autosummary',  # Create neat summary tables for modules/classes/methods etc
+              #'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
               'sphinx.ext.doctest',
               'sphinx.ext.todo',
               'sphinx.ext.mathjax',    
               'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode',
-              'nbsphinx',
+              'sphinx.ext.viewcode', # Add a link to the Python source code for classes, functions etc.
+              'nbsphinx', # Integrate Jupyter Notebooks and Sphinx
               ]
 
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
+# Mappings for sphinx.ext.intersphinx. Projects have to have Sphinx-generated doc! (.inv file)
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # replace "view page source" with "edit on github" in Read The Docs theme
