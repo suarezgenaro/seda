f01b5db6e3957d4e30f932f5f80ccf053e795572
Genaro <gsuarez2405@gmail.com>
2025-01-02 16:26:10 -0500

    test

--- seda/__init__.py
+++ seda/__init__.py
@@ -4,5 +4,5 @@ from .utils import *
 from .chi2_fit import *
 from .bayes_fit import *
 from .synthetic_photometry.synthetic_photometry import *
+from .spectral_indices.spectral_indices import *
 #from .synthetic_photometry_test.synthetic_photometry import *
-#from .spectral_indices.spectral_indices import *
