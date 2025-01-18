baef345dafcfff5a78f57971a4cfe234015a85f0
Genaro <gsuarez2405@gmail.com>
2025-01-17 19:47:46 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ from setuptools import setup
 with open("README", 'r') as f:
     long_description = f.read()
 
+#def install_requires():
+#    reqs = []
+#    for line in open('requirements.txt', 'r').readlines():
+#        reqs.append(line)
+#    return reqs
+
 setup(name='seda',
       version='1.0',
       description='SEDA: Spectral Energy Distribution Analyzer',
-      long_description='Spectral Energy Distribution Analyzer for forward modeling and empirical analysis of spectrophotometry for ultracool objects',
-      long_description=long_description
-      keywords = ['ultracool dwarfs', 'low mass stars', 'brown dwarfs', 'gas planets', \
+      long_description=long_description, 
+      keywords = ['ultracool objects', 'low mass stars', 'brown dwarfs', 'gas giant planets', \
                   'spectroscopy', 'photometry', 'atmospheres', 'astronomy', 'astrophysics'],
       author='Genaro Suárez',
       author_email='gsuarez2405@gmail.com',
@@ -19,5 +24,6 @@ setup(name='seda',
       license='MIT',
       packages=['seda', 'seda.spectral_indices', 'seda.synthetic_photometry'],
       install_requires=['astropy','corner','dynesty','lmfit','matplotlib','numpy','scipy','specutils','spectres','tqdm','xarray'], 
+#      install_requires=install_requires(),
       zip_safe=False,
       include_package_data=True)
