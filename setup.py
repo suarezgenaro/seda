27ddd3179788bc799629abab98735eea8674a940
Genaro Suárez <91069641+suarezgenaro@users.noreply.github.com>
2025-01-28 19:25:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md', 'r') as f:
     long_description = f.read()
