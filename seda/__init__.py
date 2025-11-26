56d55109963db0e8d806d8b7a31296542db99910
kelle <kellecruz@gmail.com>
2025-11-26 16:23:03 -0500

    add fall back to version number

--- seda/__init__.py
+++ seda/__init__.py
@@ -8,4 +8,7 @@ from .phy_params import *
 from .synthetic_photometry.synthetic_photometry import *
 from .spectral_indices.spectral_indices import *
 
-from ._version import __version__
+try:
+	from ._version import __version__
+except Exception:
+	__version__ = "0.0.0+local"
