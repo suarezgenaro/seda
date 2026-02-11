b0faa42e0e7715271d3b3967c3ccd12ee47effd9
Genaro <gsuarez2405@gmail.com>
2026-02-10 22:27:19 -0500

    Test reading parameters from SVO

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,8 @@ import pytest
 import numpy as np
 
 from tests.conftest import load_model_spectra_catalog
+from unittest.mock import patch
+from astropy.table import Table, MaskedColumn
 
 def test_convolve_spectrum_identity():
 	"""
@@ -101,3 +103,49 @@ def test_convolve_spectrum_real_data(model, spectrum_file):
 	
 	# flux is finite
 	assert np.all(np.isfinite(result['flux_conv']))
+
+def test_read_SVO_params():
+	"""Verify that reading parameters from the SVO table return the expected values"""
+
+	filters = ['2MASS/2MASS.J', 'Spitzer/IRAC.I1', 'WISE/WISE.W3', 'JWST/MIRI.F1000W']
+	wavelength_eff = np.array([12350.0, 35074.83347149, 115608.0, 98793.44853281])
+	width_eff = np.array([1624.3190191, 6836.18018646, 55055.230027, 17036.67237661])
+	zero_point = np.array([1594.0, 274.53023427, 31.674, 38.50820313])
+
+	# make masked fake SVO table to mimic the original one
+	fake_svo_table = Table([
+		MaskedColumn(filters, name='filterID'),
+		MaskedColumn(wavelength_eff, name='WavelengthEff'),
+		MaskedColumn(width_eff, name='WidthEff'),
+		MaskedColumn(zero_point, name='ZeroPoint')
+	])
+
+	filters_req = ['2MASS/2MASS.J', 'JWST/MIRI.F1000W']  # subset of table
+	params_req = ['WavelengthEff', 'ZeroPoint']		  # subset of columns
+
+	# select the desired parameters from the fake table
+	with patch('seda.utils.read_SVO_table', return_value=fake_svo_table):
+		result = seda.utils.read_SVO_params(
+			filters=filters_req,
+			params=params_req,
+		)
+
+	# check that all requested columns are returned
+	assert set(result) == set(params_req), (
+		f'Returned columns {list(result.keys())} do not match requested {params_req}'
+	)
+
+	# find indices of requested filters in the fake table
+	indices = []
+	for filt in filters_req:
+		# find where the filter matches in the table
+		match_index = np.where(fake_svo_table['filterID'] == filt)[0][0]
+		indices.append(match_index)
+
+	# check that values match
+	assert np.allclose(result['WavelengthEff'], wavelength_eff[indices]), (
+		f"WavelengthEff mismatch: got {result['WavelengthEff']}, expected {wavelength_eff[indices]}"
+	)
+	assert np.allclose(result['ZeroPoint'], zero_point[indices]), (
+			f"ZeroPoint mismatch: got {result['ZeroPoint']}, expected {zero_point[indices]}"
+	)
